aef12ae13e933ce78d1f8ded046e6aad427252c0
GaelS <servauga@gmail.com>
2018-05-07 13:29:22 +0200

    fix prevent clicking through disclaimer loading overlay

--- src/components/Disclaimer.js
+++ src/components/Disclaimer.js
@@ -13,44 +13,48 @@ class Disclaimer extends React.Component {
     return (
       <React.Fragment>
         <div
-          className={`login-popup-container ${this.state.waitingForAuthentication ? 'loading-overlay' : ''}`}
+          className={
+            this.state.waitingForAuthentication ? 'loading-overlay' : ''
+          }
         >
-          {!this.state.waitingForAuthentication &&
-            <div className="login-popup">
-              <h2 className="card-title-project disclaimer-title">
-                Disclaimer
-              </h2>
-              <h2 className="card-title-issue">
-                Your Github credentials are required !
-              </h2>
-              <p className="disclaimer-content">
-                This app is powered by the Graphql Github API which requires to be authenticated to be used... That's all !
-                {' '}
-              </p>
-              <div className="buttons-container">
-                <a
-                  href={process.env.AUTH_URL}
-                  className="button-ok"
-                  title="Go to the app !"
-                  onClick={e =>
-                    this.setState({ waitingForAuthentication: true })}
-                >
-                  Ok
-                </a>
-                <a
-                  className="button-nope"
-                  title="Go back to Github"
-                  href="http://github.com"
-                >
-                  Never !
-                </a>
-              </div>
-              <img
-                className="disclaimer-palm-tree"
-                src={palmTree}
-                alt="Palm Tree"
-              />
-            </div>}
+          <div className="login-popup-container">
+            {!this.state.waitingForAuthentication &&
+              <div className="login-popup">
+                <h2 className="card-title-project disclaimer-title">
+                  Disclaimer
+                </h2>
+                <h2 className="card-title-issue">
+                  Your Github credentials are required !
+                </h2>
+                <p className="disclaimer-content">
+                  This app is powered by the Graphql Github API which requires to be authenticated to be used... That's all !
+                  {' '}
+                </p>
+                <div className="buttons-container">
+                  <a
+                    href={process.env.AUTH_URL}
+                    className="button-ok"
+                    title="Go to the app !"
+                    onClick={e =>
+                      this.setState({ waitingForAuthentication: true })}
+                  >
+                    Ok
+                  </a>
+                  <a
+                    className="button-nope"
+                    title="Go back to Github"
+                    href="http://github.com"
+                  >
+                    Never !
+                  </a>
+                </div>
+                <img
+                  className="disclaimer-palm-tree"
+                  src={palmTree}
+                  alt="Palm Tree"
+                />
+              </div>}
+          </div>
         </div>
         {this.state.waitingForAuthentication && <LoadingSpinner />}
       </React.Fragment>
